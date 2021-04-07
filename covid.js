543e430f60b0b5191e518ece3f3b5c904be9d727
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-04-07 16:52:26 +0200

    Added new Stats

--- covid.js
+++ covid.js
@@ -1,3 +1,6 @@
+
+var index = -1;
+
 function update() {
     var url = 'https://corona.lmao.ninja/v2/countries/Germany';
     var noise = new Audio('soundscapes/ocean_waves.mp3');
@@ -6,13 +9,41 @@ function update() {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            document.getElementById("Output").innerHTML = "Erkrankt "+data.active;
+            switch (index) {
+                case 0:
+                    document.getElementById("Output").innerHTML = "Infiziert "+data.active;
+                    break;
+                case 1:
+                    document.getElementById("Output").innerHTML = "Geheilt "+data.recovered;
+                    break;
+                case 2:
+                    document.getElementById("Output").innerHTML = "Gestorben "+data.deaths;
+                    break;
+            }
             console.log("Refreshed everything!");
         });
 }
 
-function autoRefresh( t ) {
-    setTimeout("location.reload(true);", t);
+function next() {
+    switch (index) {
+        case 0:
+            index++;
+            document.getElementById("Update").innerHTML = "Gestorben"
+            break;
+        case 1:
+            index++;
+            document.getElementById("Update").innerHTML = "Infiziert"
+            break;
+        default:
+            index = 0;
+            document.getElementById("Update").innerHTML = "Geheilt"
+            break;
+    }
+    update()
 }
 
-update()
+function autoRefresh( t ) {
+    setTimeout("next();autoRefresh(25000)", t);
+}
+
+next()
